a59e622ee3e2607e0281cb29285848536ce37f8d
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 14:03:25 -0600

    task 5 sums a list of floats and returns float using annotation

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Function takes a list of floats
+    as arguments and returns their
+    sum as a float'''
+
+
+def sum_list(input_list: list) -> float:
+    '''input_list is a list of floats,
+    calculate their sum and return a float(total)'''
+    total = sum(input_list)
+    return total
