52f40aaf637416165bccf88f61a9046015d77fed
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 13:57:17 -0600

    task 4 defining and annotating variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Script defines some variables
+    and uses annotation on them
+    to hint their typing
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
