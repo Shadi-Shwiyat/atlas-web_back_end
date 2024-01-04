fba001ee3f2949b303c1ecfe38bb450b1d3f5810
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 14:26:19 -0600

    fixing task 6 import issue

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,7 @@
 '''Function takes a mixed list
     of ints and floats and
     returns their sum as a float'''
-from typing import *
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
