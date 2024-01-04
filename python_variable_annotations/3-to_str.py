bbb0b990e0f76a37c5bcfdc3c213bb1d22909883
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 13:50:42 -0600

    task 3 converting a float to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Function takes a float as argument
+    and returns the string representation
+    of that float
+'''
+
+
+def to_str(n: float) -> str:
+    '''N will be passed as float, converted
+    to a string using str() class and returned using
+    annotation to hint typing'''
+
+    convertedString = str(n)
+    return convertedString
