799348eb00873a3932975c40a2f5a614f434f5f3
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 13:00:51 -0600

    task 1 concatenate two strings with annotation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Function takes two strings as
+    arguments and returns concatenated strings
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Annotate string 1 and 2 to hint string
+    typing as well as return value will be string'''
+    return (str1 + str2)
