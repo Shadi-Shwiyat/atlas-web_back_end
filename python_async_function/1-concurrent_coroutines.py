cdc6cad35bb95faed8f38fc3b5164f9de8748f2c
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 17:47:49 -0600

    fixed pycodestyle issue

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''n and max_delay should be passed as an int,
     and this function will return a list of floats
