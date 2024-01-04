84b918840108f3ed87d3d5bce812ca26e23c3bc0
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 14:22:42 -0600

    task 6 using annotation on function that returns sum of mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Function takes a mixed list
+    of ints and floats and 
+    returns their sum as a float'''
+from typing import *
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Annotate mxd_lst as a union list of ints
+    and floats to accept either, sum the list and
+    return the value as a float'''
+    list_sum = sum(mxd_lst)
+    return list_sum
