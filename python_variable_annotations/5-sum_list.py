e8f109f799a2f0aaf6fe1ab47039a4085c035270
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-04 14:13:53 -0600

    fixed list typing isse task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 '''Function takes a list of floats
     as arguments and returns their
-    sum as a float
+    sum as a float. import typing module
+    to use list typing of floats
 '''
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''input_list is a list of floats,
     calculate their sum and return a float(total)'''
     total = sum(input_list)
