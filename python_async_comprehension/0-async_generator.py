f398cee1afadf021f3293a97e1e5adf6c000d71c
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-05 15:18:29 -0600

    fixed task 0 import generator typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 '''Script create an async generator'''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     '''function loops 10 times, each time
     has an asynchronous wait for 1 second, and yeild
     a random float between 0 and 10'''
