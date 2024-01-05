7da707ca2691c455f488a894eb791c02b3093551
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-05 16:12:58 -0600

    fixing task 2 issue

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,11 +8,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''Function will run async_comprehension
-    four times in parallel, measure the total
+    '''measure the total
     runtime, and return the value as a float'''
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
-    total_time = end_time - start_time
-    return total_time
+    return end_time - start_time
