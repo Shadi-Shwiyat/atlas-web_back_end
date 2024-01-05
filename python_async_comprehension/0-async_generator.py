41f82e997f462e5d41b6a734399c4bf180640690
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-05 15:13:32 -0600

    fixed task 0 using async for loop

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ async def async_generator() -> float:
     '''function loops 10 times, each time
     has an asynchronous wait for 1 second, and yeild
     a random float between 0 and 10'''
-    for i in range(10):
+    async for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
