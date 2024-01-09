a9bd9a6b2047464fdf1c22cc49f0e0a93a6086cc
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-09 17:00:59 -0600

    fixed task 1 deleting wrong key value pair

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 '''Script creates a caching system
     using inheritance from BaseCaching
-    and follows a FIFO cache replacement
+    and follows a LIFO cache replacement
     policy'''
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     '''Class inherits from BaseCaching to
-        modify its put and get methods. Uses a FIFO
+        modify its put and get methods. Uses a LIFO
         caching replacement policy'''
     def __init__(self):
         '''Call init method of parent class for access to
@@ -21,15 +21,15 @@ class FIFOCache(BaseCaching):
             None, method does nothing'''
         if key is None or item is None:
             return None
-        elif len(self.cache_data) is BaseCaching.MAX_ITEMS and key not in self.cache_data:
+        elif len(self.cache_data) is BaseCaching.MAX_ITEMS:
             # print(len(self.cache_data))
-            # Use list method to get the first key/value pair
+            # Use list method to get the last key/value pair
             # by insertion order
-            first_key, first_value = list(self.cache_data.items())[0]
-            # print(first_key)
-            del self.cache_data[first_key]
+            last_key, last_value = list(self.cache_data.items())[BaseCaching.MAX_ITEMS - 1]
+            print(list(self.cache_data.items())[BaseCaching.MAX_ITEMS - 1])
+            del self.cache_data[last_key]
             self.cache_data[key] = item
-            print(f'DISCARD:', first_key)
+            print(f'DISCARD:', last_key)
         else:
             self.cache_data[key] = item
 
