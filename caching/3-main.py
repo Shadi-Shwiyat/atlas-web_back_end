d1887fc4855f1c0699a506caa031ba873ce3a635
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-09 22:00:33 -0600

    fixing task 3 caching error

--- caching/3-main.py
+++ caching/3-main.py
@@ -1,46 +1,37 @@
 #!/usr/bin/python3
-""" 3-main """
-LRUCache = __import__('3-lru_cache').LRUCache
+"""
+Test
+"""
+import sys
 
-my_cache = LRUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-# print()
+try:
+    LRUCache = __import__('3-lru_cache').LRUCache
+    from base_caching import BaseCaching
 
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-# print()
+    BaseCaching.MAX_ITEMS = 5
+    LRUCache.MAX_ITEMS = 5
+    my_cache = LRUCache()
+    my_cache.MAX_ITEMS = 5
+    prev_key = None
 
-my_cache.put("C", "Street")
-my_cache.print_cache()
-# print()
+    for i in range(10):
+        key = "key-{}".format(i)
+        value = "value-{}".format(i)
+        if prev_key is not None:
+            my_cache.get(prev_key)
+        prev_key = key
+        my_cache.put(key, value)
+        my_cache.print_cache()
+    
+    my_cache.get("key-0")
+    my_cache.get("key-4")
+    my_cache.get("key-6")
+    my_cache.get("key-7")
+    my_cache.print_cache()
+    my_cache.put("key-20", "value-20")
+    my_cache.put("key-21", "value-21")
+    my_cache.put("key-22", "value-22")
+    my_cache.print_cache()
 
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-# print()
-
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-# print()
-
-my_cache.put("H", "H")
-my_cache.print_cache()
-# print()
-
-my_cache.put("I", "I")
-my_cache.print_cache()
-# print()
-
-my_cache.put("J", "J")
-my_cache.print_cache()
-# print()
-
-my_cache.put("K", "K")
-my_cache.print_cache()
+except:
+    print(sys.exc_info()[1])
