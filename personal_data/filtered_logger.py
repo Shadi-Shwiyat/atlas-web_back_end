b638bbf9d46078200ff6d095a2d40cbe34ade285
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-15 14:14:53 -0600

    task 1 implementing format method into given class

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
     log statement'''
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -18,3 +19,26 @@ def filter_datum(fields: List[str], redaction: str,
         replacement = rf'\1{redaction}{separator}'
         message = re.sub(pattern, replacement, message)
     return(message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''method filters values of incoming log
+            records using parent class .format
+            method'''
+        log_message = super().format(record)
+        # print(log_message)
+        result = filter_datum(self.fields, self.REDACTION,
+                              log_message, self.SEPARATOR)
+        return(result)
