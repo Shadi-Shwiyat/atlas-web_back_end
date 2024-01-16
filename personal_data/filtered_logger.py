72e0741ca3fe6e8e5e13e0b9651c0796e8aa6946
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-15 20:36:30 -0600

    fixing annotation error

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -69,7 +69,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     # Obtain credentials from environment variables
     db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
