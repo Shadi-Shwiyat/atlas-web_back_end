bca3e87baff277f5cd46d708e6d9f2295087508f
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-16 21:03:03 -0600

    fixed task 3 style issue

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,6 @@ class Auth():
     def __init__(self) -> None:
         '''Constructor method of auth class'''
 
-
     def require_auth(self, path: str,
                      excluded_paths: List[str]) -> bool:
         '''Public method that
@@ -21,14 +20,12 @@ class Auth():
             requires authorization'''
         return False
 
-
     def authorization_header(self,
                              request=None) -> str:
         '''Public method that
             will return the auth
             header'''
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''Public method will return
