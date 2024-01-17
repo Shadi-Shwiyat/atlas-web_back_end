b62d1983bccfa7769e01850bcf6451eb5be6a7c7
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-16 20:01:56 -0600

    task 1 adding error handler route

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    '''use abort to call
+      the unauthorized error
+      handler message'''
+    abort(401)
