04afd5171b36436f1e4e38917ce1a416d62042ee
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-23 17:41:16 -0600

    task 7 using flask route to register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 '''Simple flask app'''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
     '''Returns json paylod with message'''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''Route that takes form data and registers users'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        auth.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
