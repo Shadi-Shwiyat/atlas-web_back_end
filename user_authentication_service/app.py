f6051e898336f37affe218c26c75e308c9711422
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-23 21:31:41 -0600

    task 17 get reset token route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -67,7 +67,7 @@ def profile():
         abort(403)
 
 
-@app.route('reset_password', methods=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     '''Returns the users reset token'''
     email = request.form.get('email')
