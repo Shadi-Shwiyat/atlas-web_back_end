16b11c59c4b140694eafd2a4ef9f80e95279aa15
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-23 21:08:37 -0600

    task 16 generating reset password token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -104,9 +104,9 @@ class Auth():
             new uuid'''
         try:
             user = self._db.find_user_by(email=email)
-            if user:
-                reset_token = _generate_uuid()
-                self._db.update_user(user.id, reset_token=reset_token)
-                return reset_token
         except (NoResultFound, InvalidRequestError):
             raise ValueError
+        if user:
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
