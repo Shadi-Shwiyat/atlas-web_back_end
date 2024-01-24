d371eadefa052c0f617e694fb18c56f4bb5e56ca
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-23 19:04:50 -0600

    task 11 defines app route to log user in if supplied valid credentials

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 '''Simple flask app'''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from auth import Auth
 
 app = Flask(__name__)
@@ -23,6 +25,23 @@ def users():
         return jsonify({"email": f"{email}", "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''Uses email and password to log user in'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    is_valid = auth.valid_login(email, password)
+
+    if is_valid:
+        session_id = auth.create_session(email)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        return abort(401)
 
 
 if __name__ == "__main__":
