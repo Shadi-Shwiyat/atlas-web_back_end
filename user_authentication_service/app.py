4c2314f7eb47735b72235a53e5dc7a18d1923be6
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-23 21:33:55 -0600

    task 17 get reset token route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -76,7 +76,7 @@ def get_reset_password_token():
     except (NoResultFound, InvalidRequestError, ValueError):
         abort(403)
     if user:
-        reset_token = AUTH.get_reset_password_token()
+        reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
