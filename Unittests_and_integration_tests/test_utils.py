f3796412a40f29e548a3b94e2610b5fc164d4828
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-28 18:39:30 -0600

    task 3 using memoization

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -68,24 +68,53 @@ class TestGetJson(unittest.TestCase):
                       mock_requests: MagicMock
                       ) -> None:
         '''Method asserts that output of
-            access_nested_map is as expected'''
+            get_json is as expected and mocks
+            the request output in get_json'''
         mock_requests.return_value.json.return_value = payload
         result = get_json(url)
         self.assertEqual(result, payload)
 
-    # @parameterized.expand([
-    #     ({}, ('a',), (KeyError)),
-    #     ({'a': 1}, ('a', 'b'), (KeyError))
-    # ])
-    # def test_access_nested_map_exception(self,
-    #                                      nested_map: Mapping,
-    #                                      path: Sequence,
-    #                                      expected_result: Any
-    #                                      ) -> None:
-    #     '''Method catches any errors
-    #         raised by access_nested_map'''
-    #     with self.assertRaises(expected_result):
-    #         access_nested_map(nested_map, path)
+
+class TestMemoize(unittest.TestCase):
+    '''Inherits from testcase,
+        unittest class for memoize
+        function'''
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+
+    @patch('test_utils.TestClass.a_method')
+    def test_memoize(self,
+                     mock_method: MagicMock
+                     ) -> None:
+        '''Method asserts that output of
+            memoize is as expected'''
+
+        class TestClass:
+            '''Class setup to test memoize function'''
+            def __init__(self, prev_value=None):
+                '''Constructor method'''
+                self.prev_value = prev_value
+
+            def a_method(self):
+                '''Arbituary method to return
+                the meaning of life'''
+                return 42
+
+            @memoize
+            def a_property(self):
+                '''Returns value of a_method if
+                it is not already memoized'''
+                if not self.prev_value:
+                    self.prev_value = self.a_method
+                    return self.prev_value
+                else:
+                    return self.prev_value
+
+        mock_method.return_value = 42
+        test_class = TestClass()
+        test_class.a_property()
+        test_class.a_property()
+        mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
