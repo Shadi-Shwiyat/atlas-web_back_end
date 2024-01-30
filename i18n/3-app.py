b09e91bca4f1d593656cb965297d3b31b5894633
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-30 16:56:21 -0600

    task 3 parameterize templates

---
+++ i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Basic flask app with one route'''
+from flask import Flask, render_template, request
+from flask_babel import Babel, _, get_locale
+
+
+class Config:
+    '''Setup configuration for flask babel'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
+
+
+def get_locale():
+    '''Determine the best-matching language using request.accept_languages'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello():
+    '''Basic initial route
+    returns hello world'''
+    home_title = 'title'
+    home_header = 'header'
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
