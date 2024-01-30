531e1b1b2ec6f5add6ba015dc507e18d926237a3
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-30 17:21:10 -0600

    task 4 Force locale with URL parameter

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -20,8 +20,10 @@ def get_locale():
     '''Determine the best-matching language using request.accept_languages'''
     locale = request.args.get('locale')
     if not locale or locale not in app.config['LANGUAGES']:
+        print('default locale is:',request.accept_languages.best_match(app.config['LANGUAGES']))
         return request.accept_languages.best_match(app.config['LANGUAGES'])
     else:
+        print('locale is:',locale)
         return locale
 
 
@@ -32,8 +34,6 @@ babel.init_app(app, locale_selector=get_locale)
 def hello():
     '''Basic initial route
     returns hello world'''
-    home_title = 'title'
-    home_header = 'header'
     return render_template('4-index.html')
 
 
