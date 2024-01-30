370f0c6f33bb8331e0c89c6950c68006a0d16be2
Shadi-Shwiyat <raz611103@gmail.com>
2024-01-30 15:09:48 -0600

    task 0 basic flask app

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Basic flask app with one route'''
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello():
     '''Basic initial route
     returns hello world'''
-    return 'Hello World'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
