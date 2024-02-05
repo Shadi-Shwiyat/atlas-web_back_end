a715b820e81120b23968cd4ba76470cc28bf2969
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-05 15:24:00 -0600

    task 1 recovering original type using redis.get

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Simple redis caching system'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -24,3 +24,31 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> bytes:
+        '''Gets data from db and returns as
+        byte string'''
+        if fn:
+            value = self._redis.get(key)
+            if not value:
+                return None
+
+            formatted_value = fn(value)
+            return formatted_value
+        else:
+            value = self._redis.get(key)
+            if not value:
+                return None
+
+            return value
+
+    def get_str(self, value: bytes) -> str:
+        '''Returns bytes string converted
+        to regular string'''
+        decoded_string = value.decode('utf-8')
+        return decoded_string
+
+    def get_int(self, value: bytes) -> int:
+        '''Returns bytes converted to int'''
+        decoded_int = int(value)
+        return decoded_int
