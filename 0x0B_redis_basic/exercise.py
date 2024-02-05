3fd9e0a453d4b0b4fb95fcf5ea3c1549c9ba17c1
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-05 16:36:48 -0600

    task 4 retrieving lists using replay function

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -38,6 +38,23 @@ def call_history(method: Callable) -> Callable:
     return store_io
 
 
+def replay(method: Callable) -> None:
+    '''Displays history of calls
+    of a particulart function'''
+    key = method.__qualname__
+    input_key = method.__qualname__ + ':inputs'
+    output_key = method.__qualname__ + ':outputs'
+
+    total_calls = method.__self__._redis.get(key)
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+
+    print(f"{key} was called {int(total_calls)} times:")
+    for input_str, output_str in zip(inputs, outputs):
+        print(f"{key}(*{input_str.decode('utf-8')}) ->\
+              {output_str.decode('utf-8')}")
+
+
 class Cache():
     '''Simple cache system using
     redis'''
