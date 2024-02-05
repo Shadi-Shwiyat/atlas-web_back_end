8656571d6ce5c0c9d6c295c49eb355f2692806e7
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-05 15:08:32 -0600

    created repo and task 0 simple storing of strings

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Simple redis caching system'''
 import redis
 import uuid
-from typing import Any
+from typing import Union
 
 
 class Cache():
@@ -14,7 +14,10 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union(str,
+                                bytes,
+                                int,
+                                float)) -> str:
         '''Returns stringified version
         of the passed in data, stores data
         using random key from uuid()'''
