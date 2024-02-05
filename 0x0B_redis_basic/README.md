51f10e50bc00eb680c686bc696af04849f06df56
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-05 14:55:20 -0600

    created repo and task 0 simple storing of strings

---
+++ 0x0B_redis_basic/README.md
@@ -0,0 +1,6 @@
+# Redis basic
+This project delves into the basics of redis
+
+## Learning Objectives
+- Learn how to use redis for basic operations
+- Learn how to use redis as a simple cache
---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Simple redis caching system'''
+import redis
+import uuid
+from typing import Any
+
+
+class Cache():
+    '''Simple cache system using
+    redis'''
+    def __init__(self) -> None:
+        '''Create private instance
+        of redis client and flush it'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        '''Returns stringified version
+        of the passed in data, stores data
+        using random key from uuid()'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
