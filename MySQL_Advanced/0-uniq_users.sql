963a6695cd468109a619b6b47e6e93d5ce93da1d
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-05 18:52:21 -0600

    fix comment issue

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,6 @@
+-- Script creates users table if it
+-- does not exist, sets columns to
+-- id, email (unique), and name
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
