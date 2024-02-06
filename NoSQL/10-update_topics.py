7e6c13d438dcd27e359b3a055925627e8bbd7197
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-06 05:03:25 -0600

    tsk 10 update school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''MongoDB python scripting for practice'''
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics) -> None:
+    '''Changes all topics of a school
+        document based on the name'''
+    filter_query = {'name': name}
+    update_query = {'$set': {'topics': topics}}
+    mongo_collection.update_one(filter_query, update_query)
