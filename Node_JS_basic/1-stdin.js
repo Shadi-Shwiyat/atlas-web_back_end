45093fcbd4051d1b04f1100257275b6e77b4c9ac
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-17 19:06:58 -0600

    lint error

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,24 +2,24 @@
 // if not passed in through cli
 
 // Check if program is run without piped in command
-if (process.argv.length == 2 && process.stdin.isTTY) {
-  console.log("Welcome to Holberton School, what is your name?");
+if (process.argv.length === 2 && process.stdin.isTTY) {
+  console.log('Welcome to Holberton School, what is your name?');
 
   // Listen for user input
   process.stdin.on('readable', () => {
-      const userName = process.stdin.read();
-      if (userName !== null) {
-          process.stdout.write(`Your name is: ${userName}`);
-      }
+    const userName = process.stdin.read();
+    if (userName !== null) {
+        process.stdout.write(`Your name is: ${userName}`);
+    }
   });
 } else {
   // Listen for data on stdin stream (echo command pipe)
   process.stdin.on('data', (data) => {
     // console.log('about to trim');
-    const userName = data.toString().trim()
+    const userName = data.toString().trim();
 
-    console.log("Welcome to Holberton School, what is your name?");
+    console.log('Welcome to Holberton School, what is your name?');
     console.log('Your name is:', userName);
-    console.log("This important software is now closing");
+    console.log('This important software is now closing');
   });
 }
