286452d5d65b225b15f634f4ead78504875d0095
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-17 20:54:07 -0600

    task 3 convert previous function to asynchronous method

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,41 +3,45 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-
-    // Log the number of students
-    const rows = data.split('\n');
-    const rowCount = rows.length - 1;
-    console.log(`Number of students: ${rowCount}`);
-
-    // Log number of students in cs
-    let csStudents = 0;
-    const csStudentsList = [];
-    for (const row of rows) {
-      const fields = row.split(',');
-      if (fields[3] === 'CS') {
-        csStudents += 1;
-        const csStudentsName = fields[0];
-        csStudentsList.push(csStudentsName);
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-    }
-    console.log(`Number of students in CS: ${csStudents}. List: ${csStudentsList.join(', ')}`);
-
-    // Log number of students in swe
-    let sweStudents = 0;
-    const sweStudentsList = [];
-    for (const row of rows) {
-      const fields = row.split(',');
-      if (fields[3] === 'SWE') {
-        sweStudents += 1;
-        const sweStudentsName = fields[0];
-        sweStudentsList.push(sweStudentsName);
+  
+      // Log the number of students
+      const rows = data.split('\n');
+      const rowCount = rows.length - 2;
+      console.log(`Number of students: ${rowCount}`);
+  
+      // Log number of students in cs
+      let csStudents = 0;
+      const csStudentsList = [];
+      for (const row of rows) {
+        const fields = row.split(',');
+        if (fields[3] === 'CS') {
+          csStudents += 1;
+          const csStudentsName = fields[0];
+          csStudentsList.push(csStudentsName);
+        }
       }
-    }
-    console.log(`Number of students in SWE: ${sweStudents}. List: ${sweStudentsList.join(', ')}`);
+      console.log(`Number of students in CS: ${csStudents}. List: ${csStudentsList.join(', ')}`);
+  
+      // Log number of students in swe
+      let sweStudents = 0;
+      const sweStudentsList = [];
+      for (const row of rows) {
+        const fields = row.split(',');
+        if (fields[3] === 'SWE') {
+          sweStudents += 1;
+          const sweStudentsName = fields[0];
+          sweStudentsList.push(sweStudentsName);
+        }
+      }
+      console.log(`Number of students in SWE: ${sweStudents}. List: ${sweStudentsList.join(', ')}`);
+      resolve();
+    });
   });
 }
 
