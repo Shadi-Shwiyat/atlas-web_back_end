272e73e819534c98fd69f82ed2c920d258bd4e24
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-18 15:16:44 -0600

    task 4 simple http server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,10 +6,7 @@ const http = require('http');
 // Create server and listen on port 1245
 const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!');
-  // console.log(res.statusCode);
   res.end();
 });
 
 app.listen(1245);
-
-module.exports = app;
