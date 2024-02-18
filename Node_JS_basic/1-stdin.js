56e11ccaa1556537813a8b0341711af82f3d4538
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-17 19:22:24 -0600

    task 1 lint fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,13 +5,13 @@ process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', function() {
-  var userName = process.stdin.read();
+process.stdin.on('readable', () => {
+  const userName = process.stdin.read();
   if (userName !== null) {
     process.stdout.write(`Your name is: ${userName}`);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
