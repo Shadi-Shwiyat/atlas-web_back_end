42d25dbc92afceb183b19c551ffd865f53be0773
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-17 19:01:28 -0600

    task 1 using process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,3 +1,26 @@
 // Executed in command line, prompts user for name
 // if not passed in through cli
 
+
+// Check if program is run without piped in command
+if (process.argv.length == 2 && process.stdin.isTTY) {
+    console.log("Welcome to Holberton School, what is your name?");
+
+    // Listen for user input
+    process.stdin.on('readable', () => {
+        const userName = process.stdin.read();
+        if (userName !== null) {
+            process.stdout.write(`Your name is: ${userName}`);
+        }
+    });
+} else {
+    // Listen for data on stdin stream (echo command pipe)
+    process.stdin.on('data', (data) => {
+        // console.log('about to trim');
+        const userName = data.toString().trim();
+
+        console.log("Welcome to Holberton School, what is your name?");
+        console.log('Your name is:', userName);
+        console.log("This important software is now closing");
+    });
+}
