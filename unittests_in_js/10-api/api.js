6b9b7f837b30052687a1c644d62feca24e7533c3
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-19 10:53:20 -0600

    task 10 deep equality and post integration testing

---
+++ unittests_in_js/10-api/api.js
@@ -0,0 +1,38 @@
+// Instance of a simple express app for testing
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865')
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const cartID = req.params.id;
+
+  if (isNaN(cartID)) {
+    return res.status(404).send('Cart not found. Invalid cart ID.');
+  }
+
+  res.send(`Payment methods for cart ${cartID}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  })
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.userName;
+
+  res.send(`Welcome: ${username}`);
+});
