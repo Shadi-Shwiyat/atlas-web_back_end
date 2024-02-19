670e522d5a7b38fe7060708490c467eb7383d6cb
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-19 09:14:08 -0600

    task 9 testing for new route and 404 error

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -20,12 +20,12 @@ describe('Route (/)', function () {
 
 describe('Route (/cart/:id)', function () {
   it('returns correct status code/result when id is a #', function (done) {
-    request('http://localhost:7865/cart/333', function (error, response, body) {
+    request('http://localhost:7865/cart/333', function (err, res, body) {
       if (err) {
         return done(err)
       };
 
-      expect(response.statusCode).to.equal(200);
+      expect(res.statusCode).to.equal(200);
 
       expect(body).to.equal(`Payment methods for cart 333`);
 
@@ -34,12 +34,12 @@ describe('Route (/cart/:id)', function () {
   });
 
   it('returns correct status code/result when id is NOT a # (404)', function (done) {
-    request('http://localhost:7865/cart/test', function (error, response, body) {
+    request('http://localhost:7865/cart/test', function (err, res, body) {
       if (err) {
         return done(err)
       };
 
-      expect(response.statusCode).to.equal(404);
+      expect(res.statusCode).to.equal(404);
 
       done();
     });
