65801aae01a7e0503a44de5ff75bbe66d1d3e079
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-19 06:22:18 -0600

    task 2 test suite using chai

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,49 @@
+// Script test functionality of
+//  0-calcul function
+
+const chai = require('chai');
+const calculateNumber = require('./1-calcul');
+
+const { expect } = chai;
+
+describe('calculateNumber', () => {
+  it('returns a sum', () => {
+    const result = calculateNumber('SUM', 1, 3);
+    expect(result).to.equal(4);
+  });
+
+  it('rounds first number', () => {
+    const result = calculateNumber('SUM', 1.2, 3);
+    expect(result).to.equal(4);
+  });
+
+  it('rounds second number', () => {
+    const result = calculateNumber('SUM', 1, 3.3);
+    expect(result).to.equal(4);
+  });
+
+  it('rounds both numbers', () => {
+    const result = calculateNumber('SUM', 1.2, 3.7);
+    expect(result).to.equal(5);
+  });
+
+  it('rounds a number up when .5', () => {
+    const result = calculateNumber('SUM', 1.5, 3.7);
+    expect(result).to.equal(6);
+  });
+
+  it('returns a difference', () => {
+    const result = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(result).to.equal(-4);
+  });
+
+  it('returns a quotient', () => {
+    const result = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(result).to.equal(0.2);
+  });
+
+  it('returns an error when dividing by 0', () => {
+    const result = calculateNumber('DIVIDE', 1.4, 0);
+    expect(result).to.equal('Error');
+  });
+});
