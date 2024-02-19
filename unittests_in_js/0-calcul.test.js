6c86ab5a77cdfcecb5007f6211de0d606ac3c66f
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-19 03:54:08 -0600

    task 0 basic test with mocha and assertion

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,26 +1,31 @@
 // Script test functionality of
 //  0-calcul function
 
-const assert = require('assert')
+const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('Returns a sum', function() {
+describe('calculateNumber', () => {
+  it('returns a sum', () => {
     const result = calculateNumber(1, 3);
     assert.equal(result, 4);
   });
 
-  it('Rounds second number', function() {
+  it('rounds first number', () => {
+    const result = calculateNumber(1.2, 3);
+    assert.equal(result, 4);
+  });
+
+  it('rounds second number', () => {
     const result = calculateNumber(1, 3.7);
     assert.equal(result, 5);
   });
 
-  it('Rounds both numbers', function() {
+  it('rounds both numbers', () => {
     const result = calculateNumber(1.2, 3.7);
     assert.equal(result, 5);
   });
 
-  it('Rounds a number up when .5', function() {
+  it('rounds a number up when .5', () => {
     const result = calculateNumber(1.5, 3.7);
     assert.equal(result, 6);
   });
