97350e2eb7ce0815ba44c3954748d9ac41748209
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-28 17:33:30 -0600

    task 9 job processor that tracks progress and errors

--- queuing_system_in_js/7-job_creator.js
+++ queuing_system_in_js/7-job_creator.js
@@ -29,14 +29,14 @@ for (const jobData of jobs) {
   });
 
   job.on('complete', () => {
-    console.log(`Notification job ${job.id} completed`);
+    console.log(`Notification job #${job.id} completed`);
   });
 
   job.on('failed', (err) => {
-    console.log(`Notification job ${job.id} failed: ${err}`);
+    console.log(`Notification job #${job.id} failed: ${err}`);
   });
 
   job.on('progress', (progress) => {
-    console.log(`Notification job ${job.id} ${progress}% complete`);
+    console.log(`Notification job #${job.id} ${progress}% complete`);
   });
 }
