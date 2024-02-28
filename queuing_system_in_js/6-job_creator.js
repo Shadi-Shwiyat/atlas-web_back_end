ea403893422a9743ddbc2aac6c1ab1c4369d9601
Shadi-Shwiyat <raz611103@gmail.com>
2024-02-28 16:53:51 -0600

    task 6 building a job creator for a job processor

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,30 @@
+// Creating a job creator and a queue using kue
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '9182104148',
+  message: 'This is a fake notification',
+};
+
+const job = queue.create('push_notification_code', jobData);
+
+job.save((err) => {
+  if (!err) {
+    console.log(`Notification job created: ${job.id}`);
+  } else {
+    console.error('Error creating job:', err);
+    process.exit(1);
+  }
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+  process.exit(0);
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+  process.exit(1);
+});
